ad96eac34bb290f6fc5d48222b55fec639bfc56e
kreuzerk <kevin.kreuzer90@icloud.com>
2017-05-05 07:53:40 +0200

    kk: Add Increment Decrement example

---
+++ incrementDecrement/incrementDecrement.js
@@ -0,0 +1,23 @@
+/**
+ * Created by kevinkreuzer on 05.05.17.
+ */
+
+const incrementButton = document.getElementById('increment-button')
+const decrementButton = document.getElementById('decrement-button')
+const output = document.querySelector('output')
+const BUTTON_EVENTS = Object.freeze({
+    INCREMENT: 'INCREMENT',
+    DECREMENT: 'DECREMENT'
+})
+
+const increment$ = Rx.Observable.fromEvent(incrementButton, 'click')
+    .map(e => BUTTON_EVENTS.INCREMENT)
+
+const decrement$ = Rx.Observable.fromEvent(decrementButton, 'click')
+    .map(e => BUTTON_EVENTS.DECREMENT)
+
+Rx.Observable.merge(increment$, decrement$)
+    .scan((acc, value) => {
+        return value === BUTTON_EVENTS.INCREMENT ? ++acc : --acc
+    }, 0)
+    .subscribe(value => console.log('Value', value))
