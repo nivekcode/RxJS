6af53a11a662d36198ec95c3560a2ed3214175cb
kreuzerk <kevin.kreuzer90@icloud.com>
2017-05-05 08:06:56 +0200

    kk: Added increment and decrement example

--- incrementDecrement/incrementDecrement.js
+++ incrementDecrement/incrementDecrement.js
@@ -5,6 +5,7 @@
 const incrementButton = document.getElementById('increment-button')
 const decrementButton = document.getElementById('decrement-button')
 const output = document.querySelector('output')
+output.innerHTML = 0;
 const BUTTON_EVENTS = Object.freeze({
     INCREMENT: 'INCREMENT',
     DECREMENT: 'DECREMENT'
@@ -20,4 +21,4 @@ Rx.Observable.merge(increment$, decrement$)
     .scan((acc, value) => {
         return value === BUTTON_EVENTS.INCREMENT ? ++acc : --acc
     }, 0)
-    .subscribe(value => console.log('Value', value))
+    .subscribe(value => output.innerHTML = value)
