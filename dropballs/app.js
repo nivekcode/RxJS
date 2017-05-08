388973d6a52e9ac206712acc48dae9eab7dd6d53
kreuzerk <kevin.kreuzer90@icloud.com>
2017-05-08 19:02:09 +0200

    kk: Added dropped balls counter

--- dropballs/app.js
+++ dropballs/app.js
@@ -3,15 +3,23 @@
  */
 
 let dropButton = document.getElementById('drop-ball-button')
+let dropedCounter = document.getElementById('dropped-counter')
 const dropClicks$ = Rx.Observable.fromEvent(dropButton, 'click')
 
 /*
-Contact Map --> Drops new ball when the previous ball has finished
-Switch Map --> Removes the current ball when a new ball arrives
-Merge Map --> Allows you to have multiple balls at the same time
+ Contact Map --> Drops new ball when the previous ball has finished
+ Switch Map --> Removes the current ball when a new ball arrives
+ Merge Map --> Allows you to have multiple balls at the same time
  */
+const updateDroppedCounter = (droppedBalls) => {
+    dropedCounter.innerHTML = `Dropped Balls: ${droppedBalls}`
+}
 
 dropClicks$
+    .scan((acc) => {
+        updateDroppedCounter(acc);
+        return ++acc;
+    }, 1)
     .mergeMap(_ => dropBall())
     .subscribe()
 
