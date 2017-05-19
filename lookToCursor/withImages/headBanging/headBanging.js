58c1172c7bd9f25cfbfec1671da5088a1ba14ff9
kreuzerk <kevin.kreuzer90@icloud.com>
2017-05-19 20:51:43 +0200

    kk: Change head bangig direction

--- lookToCursor/withImages/headBanging/headBanging.js
+++ lookToCursor/withImages/headBanging/headBanging.js
@@ -2,27 +2,46 @@
  * Created by kevinkreuzer on 17.05.17.
  */
 const image = document.querySelector('img')
+const changeDirectionButton = document.getElementById('change-direction')
 
 const IMAGE_NAMES = Object.freeze({
     TOP_RIGHT: 'TOP_RIGHT',
     STRAIGHT_RIGHT: 'STRAIGHT_RIGHT',
-    BOTTOM_RIGHT: 'BOTTOM_RIGHT'
+    BOTTOM_RIGHT: 'BOTTOM_RIGHT',
+    TOP_LEFT: 'TOP_LEFT',
+    STRAIGHT_LEFT: 'STRAIGHT_LEFT',
+    BOTTOM_LEFT: 'BOTTOM_LEFT'
+})
+
+const DIRECTIONS = Object.freeze({
+    RIGHT: 'RIGHT',
+    LEFT: 'LEFT'
 })
 
 const changeImage = (imageName) => image.setAttribute('src', `../images/${imageName}.jpg`)
+const halfSecondTicks$ = Rx.Observable.interval(400)
+const changeDirection$ = Rx.Observable.fromEvent(changeDirectionButton, 'click')
+const direction$ = changeDirection$
+    .map(e => DIRECTIONS.RIGHT)
+    .scan((clickValue) => {
+        return clickValue === DIRECTIONS.LEFT ? DIRECTIONS.RIGHT : DIRECTIONS.LEFT
+    }, DIRECTIONS.RIGHT)
+    .startWith(DIRECTIONS.RIGHT)
 
-Rx.Observable.interval(500)
-    .switchMap(() => {
-        changeImage(IMAGE_NAMES.TOP_RIGHT)
-        return Rx.Observable.interval(250).take(1)
+
+halfSecondTicks$.combineLatest(direction$)
+    .switchMap(tickAndDirection => {
+        const direction = tickAndDirection[1]
+        changeImage(IMAGE_NAMES[`TOP_${direction}`])
+        return Rx.Observable.interval(250).take(1).map(tick => direction)
     })
-    .do(() => {
-        changeImage(IMAGE_NAMES.STRAIGHT_RIGHT)
-        return Rx.Observable.interval(250).take(1)
+    .do(direction => {
+        changeImage(IMAGE_NAMES[`STRAIGHT_${direction}`])
+        return Rx.Observable.interval(250).take(1).map(tick => direction)
     })
-    .do(() => {
-        changeImage(IMAGE_NAMES.BOTTOM_RIGHT)
-        return Rx.Observable.interval(250).take(1)
+    .do(direction => {
+        changeImage(IMAGE_NAMES[`BOTTOM_${direction}`])
+        return Rx.Observable.interval(250).take(1).map(tick => direction)
     })
     .subscribe(() => {
     })
