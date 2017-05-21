67e1fc4ca751ceff79b076c257911a6036fae5ce
kreuzerk <kevin.kreuzer90@icloud.com>
2017-05-21 14:44:31 +0200

    kk: Add first version of the doorman example

--- doorman/doorman.js
+++ doorman/doorman.js
@@ -2,8 +2,30 @@
  * Created by kevinkreuzer on 21.05.17.
  */
 
-/*
- setInterval(() => {
- console.log(harryPotterNames.random() + ' tries to enter')
- }, 1000)
- */
+const commands = [
+    {emoji: '🕺', name: 'dancer'},
+    {emoji: '🍺', name: 'beer'},
+];
+
+// Create custom commands
+commands.forEach(({name, emoji}) => window.console[name] = (...args) => console.log(emoji + ' ' + args.join(', ')));
+
+const doorHandle = document.querySelector('#door-handle')
+const arrivalPersons$ = Rx.Observable.interval(1500)
+    .map(e => harryPotterNames.random())
+    .take(20)
+const openDoor$ = Rx.Observable.fromEvent(doorHandle, 'mousedown')
+const closeDoor$ = Rx.Observable.fromEvent(doorHandle, 'mouseup')
+
+arrivalPersons$.subscribe(
+    character => console.dancer(`${character} tries to enter....`),
+    err => console.error(err),
+    _ => console.log('No more people interested in the party')
+)
+
+arrivalPersons$
+    .windowToggle(openDoor$, () => closeDoor$)
+    .mergeAll()
+    .subscribe(character => console.beer('Letting in ', character))
+
+
