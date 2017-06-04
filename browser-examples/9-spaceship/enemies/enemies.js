3be6b37951be203b219b2a05d96723a5a7c8dc66
kreuzerk <kevin.kreuzer90@icloud.com>
2017-06-04 13:18:46 +0200

    kk: Bring movement to the enemies

--- browser-examples/9-spaceship/enemies/enemies.js
+++ browser-examples/9-spaceship/enemies/enemies.js
@@ -3,6 +3,14 @@
  */
 const ENEMIE_FREQUENCY = 2000
 
+function getOneOrZero() {
+    return Math.round(Math.random() * (1 - 0) + 0);
+}
+
+function getRandomNumberForRange(minimum, maximum) {
+    return Math.random() * (maximum - minimum) + minimum
+}
+
 const enemies$ = Rx.Observable
     .interval(ENEMIE_FREQUENCY)
     .map(tick => ({
@@ -13,3 +21,18 @@ const enemies$ = Rx.Observable
         enemies.push(newEnemie)
         return enemies
     }, [])
+    .switchMap(enemies => Rx.Observable
+        .interval(100)
+        .map(_ => {
+            enemies.forEach(enemie => {
+                if (getOneOrZero() === 0) {
+                    enemie.x += getRandomNumberForRange(1, 30)
+                }
+                else {
+                    enemie.x -= getRandomNumberForRange(1, 30)
+                }
+            })
+            return enemies
+        })
+    )
+    .startWith([])
