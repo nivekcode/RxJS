8a1f58f32f42e581b4811fbf3ce73ba8534b156a
kreuzerk <kevin.kreuzer90@icloud.com>
2018-09-14 16:14:44 +0200

    Add some marble tests for sync operators

--- marble-testing/spec/marbles.spec.ts
+++ marble-testing/spec/marbles.spec.ts
@@ -1,6 +1,7 @@
 import 'jasmine';
 import {TestScheduler} from 'rxjs/testing';
-import {throttleTime} from 'rxjs/operators';
+import {filter, map, throttleTime} from 'rxjs/operators';
+import {cold} from 'jasmine-marbles';
 
 describe('Sample marble test', () => {
 
@@ -8,16 +9,33 @@ describe('Sample marble test', () => {
         expect(actual).toEqual(expected);
     });
 
-    it('must be green', () => {
+    it('must skip the throttled value', () => {
         scheduler.run(helpers => {
             const {cold, expectObservable, expectSubscriptions} = helpers;
             const stream$ = cold('-a--b--c---|');
-            const subs =         '^----------!';
-            const expected =     '-a-----c---|';
+            const subs = '^----------!';
+            const expected = '-a-----c---|';
 
-            expectObservable(stream$.pipe(throttleTime(3, scheduler))).toBe(expected);
+            expectObservable(stream$.pipe(throttleTime(3))).toBe(expected);
             expectSubscriptions(stream$.subscriptions).toBe(subs);
         });
+    });
+
+
+    it('must add 10 to all values', () => {
+        const source$ = cold('--a--b--c--d|', {a: 5, b: 4, c: 10, d: 8});
+        const expected$ = cold('--a--b--c--d|', {a: 15, b: 14, c: 20, d: 18});
+
+        const result = source$.pipe(map((x: number) => x + 10));
+        expect(result).toBeObservable(expected$)
+    });
+
+    it('must filter out values', () => {
+        const source$ = cold('--a--b--c--d|', {a: 1, b: 2, c: 3, d: 4});
+        const expectedDiagram$ = cold('-----b-----d|', {b: 2, d: 4});
+
+        const result = source$.pipe(filter((x: number) => x % 2 === 0));
+        expect(result).toBeObservable(expectedDiagram$);
     });
 
 });
