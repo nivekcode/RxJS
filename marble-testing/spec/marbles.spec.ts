49e841310d21c4f831f4abdd9927914cacafa235
kreuzerk <kevin.kreuzer90@icloud.com>
2018-09-14 18:12:20 +0200

    Add test for switch map

--- marble-testing/spec/marbles.spec.ts
+++ marble-testing/spec/marbles.spec.ts
@@ -1,6 +1,6 @@
 import 'jasmine';
 import {TestScheduler} from 'rxjs/testing';
-import {filter, map, throttleTime} from 'rxjs/operators';
+import {filter, map, switchMap, take, throttleTime} from 'rxjs/operators';
 import {cold} from 'jasmine-marbles';
 
 describe('Sample marble test', () => {
@@ -36,6 +36,29 @@ describe('Sample marble test', () => {
 
         const result = source$.pipe(filter((x: number) => x % 2 === 0));
         expect(result).toBeObservable(expectedDiagram$);
+    });
+
+    it('must map each value to an inner observable', () => {
+        const source$ = cold('--a--b--c-d|');
+        const inner$ = cold('x--yz|');
+        const expected$ = cold('--x--x--x-x--yz|');
+
+        const result$ = source$.pipe(switchMap(() => inner$));
+        expect(result$).toBeObservable(expected$);
+    });
+
+    it('must add each value from the outer to the inner observable and return the result', () => {
+        const values = {a: 5, b: 10, c: 15, d: 20, e: 25, f: 30};
+        const source$ = cold(   '---a--ba-b---a', values);
+        const inner$ = cold(    'c-dca', values);
+        const expected$ = cold('---d-eed-e-fed-edb', values);
+
+        const result$ = source$.pipe(
+            switchMap((x: number) => inner$.pipe(
+                map((y: number) => x + y)
+            ))
+        );
+        expect(result$).toBeObservable(expected$);
     });
 
 });
